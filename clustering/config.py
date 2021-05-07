03b2be4c8a6227230f193216159b8b1a92a21451
CS17B011 <cs17b011@iittp.ac.in>
2021-05-07 11:17:57 +0530

    Config Update

--- clustering/config.py
+++ clustering/config.py
@@ -1,122 +1,53 @@
-import mysql.connector as sqlc
-from sklearn.metrics import davies_bouldin_score, calinski_harabasz_score, silhouette_score
+import getopt
 
-# Input Question from MySQL Database
-def getInputQuestionSQL(username,password,tag_list,number_of_samples,host="localhost"):
-    mydb = sqlc.connect(
-        host=host,
-        user=username,
-        password=password
-    )
-    connection = mydb.cursor()
+# Clustering Parameters
+config = {
+    'number_of_samples' : 1000,
+    'tag_list' : ['javascript', 'python'],
+    'max_body_length' : 200,
+    'threshold' : 0.5,
+    'print_min_size' : 2,
+    'enable_question_print' : True,
+    'transformer_model' : 'distilbert-base-nli-stsb-mean-tokens'
+}
 
-    connection.execute("USE stackoverflow")
+# n: number of samples
+# t: threshold
+# m: model
+# p: print min size
+options = "n:t:m:p:"
 
-    sql_tag_list = ["INSTR(Tags,'<" + x + ">') > 0" for x in tag_list]
-    final_tag_condition = " || ".join(sql_tag_list)
+# nos: number os samples
+# tag-list: tag list
+# max-body: max body length
+# model: model
+# print: enable question print
+# print-min-size: print min size
+long_options = ["nos", "tag-list =", "th", "model", "print", "print-min-size"]
 
-    sql = "SELECT * FROM Posts WHERE PostTypeId=1 AND (" + str(final_tag_condition) + ") AND Body NOT LIKE '%<code>%' AND Body NOT LIKE '%<img>%' LIMIT " + str(number_of_samples)
-
-    connection.execute(sql)
-
-    row_headers = [x[0] for x in connection.description]
-    result = connection.fetchall()
-
-    return result, row_headers
-
-# Printing Information regarding Clustering 
-def printInitialInfo(nos,threshold,tag_list,transformer_model,vlen):
-
-    print("----------------------------------------------------------------------------------")
-    print("Number of Samples : ",nos)
-    print("Threshold : ",threshold)
-    print("Tag List : ",tag_list)
-    print("Model : ",transformer_model)
-    print("----------------------------------------------------------------------------------")
-
-    # Dimensions of embeded vector
-    print("Embeded Vector Dimension Info:")
-    print("Length of an array: ", nos)
-    print('Sample vector - length', vlen)
-    print("----------------------------------------------------------------------------------")
-
-# BFS for generation Clusters
-def bfs(node, mat,visited,threshold,nos):
-    cluster = [node]
-    visited[node] = True
-    index = 0
-    while index < len(cluster):
-        curr_node = cluster[index]
-        for i in range(0, nos):
-            if(i == curr_node):
-                continue
-            else:
-                if visited[i] == False and mat[curr_node][i] >= threshold:
-                    visited[i] = True
-                    cluster.append(i)
-        index += 1
-    return cluster
-
-# Utility to generate Clusters using multiple BFS call
-def getClusters(mat, nos,th):
-    visited = [False] * nos
-    labels = [0] * nos
-    cluster_index = 1
-    cluster_set = list()
-    for i in range(0, nos):
-        if visited[i] == False:
-            cluster = bfs(i, mat,visited,th,nos)
-            for ele in cluster:
-                labels[ele] = cluster_index
-            cluster_index += 1
-            cluster_set.append(cluster)
-    return cluster_set, labels
-
-# Printing Cluster Information
-def printCluster(clusters,q_arr,details=False,print_min_size=2):
-    
-    cluster_size_dict = dict({})
-    for cluster in clusters:
-        length = len(cluster)
-        if length in cluster_size_dict:
-            cluster_size_dict[length] += 1
-        else:
-            cluster_size_dict[length] = 1
-        if details:
-            if(len(cluster) >= print_min_size):
-                print("\n<========================================   CLUSTER START   ==========================================================>\n")
-                for node in cluster:
-                    print(node, " : ",q_arr[node])
-                    print("--------------------------------------------------------------------------------------------------")
-                print("\n<========================================   CLUSTER END   =========================================================>\n")
-    
-    print("\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n")
-    ("--------------------------------------------------------------------------------------------------")
-    print("Total Clusters : ", len(clusters))
-    print("--------------------------------------------------------------------------------------------------")
-
-    print("\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n")
-
-    for key in cluster_size_dict:
-        print(key, " : ",cluster_size_dict[key])
-        print("--------------------------------------------------------------------------------------------------")
-
-    print("\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n")
-
-# Printing Evaluation Score
-def printScores(embedding,labels):
+def getConfig(argumentList):
     try:
-        score = silhouette_score(embedding, labels, metric='euclidean')
-        print("Evaluation Score SH (higher better): ", score)
-    except:
-        print("Evaluation Score SH (higher better): NA")
-    try:
-        score = calinski_harabasz_score(embedding, labels)
-        print("Evaluation Score CH (higher better): ", score)
-    except:
-        print("Evaluation Score CH (higher better): NA")
-    try:
-        score = davies_bouldin_score(embedding, labels)
-        print("Evaluation Score DB (lower better): ", score)
-    except:
-        print("Evaluation Score DB (lower better): NA")
+        # Parsing argument
+        arguments, values = getopt.getopt(argumentList, options, long_options)
+
+        # checking each argument
+        for (currentArgument, currentValue) in arguments:
+            if currentArgument.strip() in ("-n", "--nos"):
+                config['number_of_samples'] = int(currentValue)
+            elif currentArgument.strip() in ("-t", "--th"):
+                config['threshold'] = float(currentValue)
+            elif currentArgument.strip() in ("-m", "--model"):
+                config['transformer_model'] = currentValue
+            elif currentArgument.strip() in ("-p", "--print-min-size"):
+                config['print_min_size'] = int(currentValue)
+            elif currentArgument.strip() == "--tag-list":
+                config['tag_list'] = list(map(str, currentValue.strip('[]').split(',')))
+            elif currentArgument.strip() == "--print":
+                config['enable_question_print'] = bool(currentValue)
+            elif currentArgument.strip() == "--print-min-size":
+                config['print_min_size'] = int(currentValue)
+        return config
+    except getopt.error as err:
+        # output error, and return with an error code
+        print (str(err))
+        return {}
